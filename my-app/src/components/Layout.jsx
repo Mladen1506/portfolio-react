54729b0eab33710abf0e318e1c0bc270d9463ec1
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-04-05 12:17:48 -0400

    intro section done

--- my-app/src/components/Layout.jsx
+++ my-app/src/components/Layout.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
+import { staticFolder } from '../utils/url-lib';
 
 const Layout = () => {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backSpeed: 50,
+      showCursor: true,
+      strings: ["Full Stack Web Developer"]
+    })
+  }, [])
+
   return (
     <div className="layout">
 
@@ -33,6 +49,51 @@ const Layout = () => {
               </span>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="layout-body">
+        <div className="sections">
+          <section className="intro" id="intro">
+            <div className="imgContainer">
+            </div>
+            <div className="wrapper">
+              <h2>Hi There, I'm</h2>
+              <h1>Mladen Makivic</h1>
+              <h3><span ref={textRef}></span><span className="ityped-cursor"></span></h3>
+              <div className="social"><a href="https://www.linkedin.com/in/julia-berezina/" target="_blank">
+                <img className="icone" src={staticFolder() + "/img/linkedin.png"} alt="" />
+              </a>
+                <a href="https://github.com/Berezka-creator" target="_blank">
+                  <img className="icone" src={staticFolder() + "/img/github.png"} alt="" />
+                </a>
+                <a href="https://www.facebook.com/yulia.berezina.16" target="_blank">
+                  <img className="icone" src={staticFolder() + "/img/facebook.png"} alt="" />
+                </a>
+                <a href="https://www.instagram.com/julia_b_samara/" target="_blank">
+                  <img className="icone inst" src={staticFolder() + "/img/insta.png"} alt="" />
+                </a>
+              </div>
+              <div className="buttons">
+                <a href="/#portfolio">Portfolio</a>
+                <a href="/resume">Resume</a>
+              </div>
+            </div>
+            <a href="#portfolio" id="arrow">
+              <img src={staticFolder() + "/img/arrow.png"} alt="" /></a>
+          </section>
+
+          <section className="portfolio">
+            portfolio
+          </section>
+
+          <section className="slider-container">
+            slider
+          </section>
+
+          <section className="contact">
+            contact
+          </section>
         </div>
       </div>
 
