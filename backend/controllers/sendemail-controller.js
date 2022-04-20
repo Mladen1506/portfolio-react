5519f7699403b41e6cb88b33b47476fbd9ff28e2
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-04-20 14:00:31 -0400

    update

--- backend/controllers/sendemail-controller.js
+++ backend/controllers/sendemail-controller.js
@@ -12,14 +12,14 @@ const sendEmailController = async (req, res, next) => {
   // using Twilio SendGrid's v3 Node.js Library
   // https://github.com/sendgrid/sendgrid-nodejs
   // javascript
-  // sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-  // const msg = {
-  //   to: 'makivic.mladen1203@gmail.com', // Change to your recipient
-  //   from: 'makivic.mladen1989@gmail.com', // Change to your verified sender
-  //   subject: 'Mladen Portfolio: Sending with SendGrid is Fun' + req.body.youremail,
-  //   text: req.body.text,
-  //   html: '<div>' + req.body.text + '</div>',
-  // };
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'makivic.mladen1203@gmail.com', // Change to your recipient
+    from: 'makivic.mladen1989@gmail.com', // Change to your verified sender
+    subject: 'Mladen Portfolio: Sending with SendGrid is Fun' + req.body.youremail,
+    text: req.body.text,
+    html: '<div>' + req.body.text + '</div>',
+  };
   // sgMail
   //   .send(msg)
   //   .then(() => {
